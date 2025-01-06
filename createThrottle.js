0135bee637a9d6c82a3212b78258518823caddef
Aryainguz <aryaninguz369@gmail.com>
2025-01-07 03:05:25 +0530

    feat: add createThrottle function for rate limiting

---
+++ createThrottle.js
@@ -0,0 +1,23 @@
+function createThrottle(func, delay) {
+    let lastExecuted = 0;
+    let timeoutId;
+  
+    const throttled = (...args) => {
+      const now = Date.now();
+      if (now - lastExecuted >= delay) {
+        func(...args);
+        lastExecuted = now;
+      } else {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+          func(...args);
+          lastExecuted = Date.now();
+        }, delay - (now - lastExecuted));
+      }
+    };
+  
+    return throttled;
+  }
+  
+  module.exports = createThrottle;
+  
